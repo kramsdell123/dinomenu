e073ffc8ff8f5cfb242d4e8130a82b1028ad5c8f
kramsdell123 <krams59@outlook.com>
2024-10-27 10:57:50 -0400

    added ui file

--- inject.js
+++ inject.js
@@ -1,13 +1,24 @@
 function main() {
+    fetch("https://raw.githubusercontent.com/kramsdell123/dinomenu/main/ui.html")
+    .then(response=>response.text())
+    .then(html=>{
+        container = document.createElement("div");
+        container.innerHTML = html;
+        document.body.appendChild(container);
+    });
+}
+function checks() {
     if(typeof Runner === "undefined") {
+        console.error("Not on dino game");
         return;
     }
     try {
         if(applied === true) {
+            console.error("Mod is already applied");
             return;
         }
     }catch(error){}
     applied = true;
-    alert("test");
+    main();
 }
-main()
+checks()
