9aa9972768208e698f0af686f3681e70adb41d89
kramsdell123 <krams59@outlook.com>
2024-10-27 20:10:14 -0400

    made a speed input!

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ document.getElementById("noclip").addEventListener("change", function() {
         Runner.instance_.gameOver = gameoverfunction;
     }
 });
+document.getElementById("speedinput").addEventListener("input", function() {
+    if(this.value < 2) {
+        this.value = 2;
+    }
+    if(this.value > 1000) {
+        this.value = 1000;
+    }
+    document.getElementById("speedslider").value = this.value;
+    Runner.instance_.currentSpeed = this.value;
+});
+document.getElementById("speedslider").addEventListener("input", function() {
+    document.getElementById("speedinput").value = this.value;
+    Runner.instance_.currentSpeed = this.value;
+});
